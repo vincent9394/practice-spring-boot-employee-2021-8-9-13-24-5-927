234e69e170716c8a391c19b51c7dd9174dff55dc
vincent <luk96515@gmail.com>
2021-10-29 00:48:33 +0800

    [SpringBootEmployeeApplication][Vincent] refactor: complete the service test without update and delete

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -0,0 +1,5 @@
+package com.thoughtworks.springbootemployee.controller;
+
+public class CompanyController {
+
+}
--- src/main/java/com/thoughtworks/springbootemployee/entity/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/entity/Employee.java
@@ -11,7 +11,6 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String name;
     private Integer age;
     private String gender;
