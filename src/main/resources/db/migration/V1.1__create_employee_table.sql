bf33b769d31d177c2bb2d347a6420d7135c5599d
vincent <luk96515@gmail.com>
2021-10-28 21:46:44 +0800

    [SpringBootEmployeeApplication][Vincent] feat: create the v1.1 employee db migration

--- src/main/resources/db/migration/V1.1__create_employee_table.sql
+++ src/main/resources/db/migration/V1.1__create_employee_table.sql
@@ -1,5 +1,8 @@
 create table employee(
-    id integer not null auto_increment primary key,
+    id int not null auto_increment primary key,
     name varchar(255) not null,
-    age integer
+    age int,
+    salary double,
+    company_id int,
+    foreign key (company_id) references company (company_id)
 )
